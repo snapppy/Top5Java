cd1bb689ff49eef71fd971e8419ae1508e01d0ef
snapppy <forever.spyder@gmail.com>
2017-12-06 15:52:18 -0700

    all

--- PracticeCode.java
+++ PracticeCode.java
@@ -1,29 +1,49 @@
-/* Author: James Warner */
-
-/* The class name JavaFiddle is used to allow it to be recognized at https://javafiddle.leaningtech.com/
-*  This will be the main driving class to test all the other classes
-*  */
+/**************************************************************************
+* The class name JavaFiddle is used to allow it to be recognized at https://javafiddle.leaningtech.com/
+* This will be the main driving class to test all the other classes
+***************************************************************************/
 public class JavaFiddle {
 
     public static void main(String[] args) {
-        User bob = new User("bobdabest");
-        User george = new User("gijoe25");
+        Student bob = new Student("bobisdabest");
+        Student george = new Student("gijoe25");
 
-        System.out.println(bob.getUsername());
-        System.out.println(george.getUsername());
+        Student jill = new Student("upthehill234", "Junior");
+
+        // Add your own Teacher class for this code to work!
+        //Teacher profSnape = new Teacher("snapesbackintown");
+
+        //Users array can hold any classes derrived from User.
+        //Create a Teacher class and give it a try!
+        User[] users = new User[] {bob, george, jill};
 
 
+        for(int i = 0; i < users.length; i++) {
+            displayUserType(users[i]);
+        }
+
+    }
+
+    public static void displayUserType(User user) {
+        //instanceof is a keyword that lets us know what type of
+        //object 'user' is. This is a handy way to custimize functionality
+        //based off of the type of object.
+        if(user instanceof Teacher) {
+            System.out.println(user.getUsername() + " is a teacher.");
+        }
     }
 
 }
 
-/* This is the User abstract class. */
+/***************************************************************
+ * This is the User abstract class.
+ ***************************************************************/
 
-class User {
+public abstract class User{
     private String username;
 
     User() {
-        username = "Unknown";
+        username = "unknown";
     }
 
     User(String username) {
@@ -33,31 +53,37 @@ class User {
     public String getUsername() {
         return username;
     }
+
 }
 
-/* This is the Student class, which inherits from User */
-class Student extends User {
-    //inherits username variable from User
+/***************************************************************
+ * This is the Student class, which inherits from User
+ ***************************************************************/
+public class Student extends User {
+    //inherits username variable from user
 
-    //add a variable just to Student called currentClass (freshman, junior, etc)
-    String currentClass;
+    //create a variable unique to Student
+    private String currentClass;
 
     Student(String username) {
         super(username);
+        //calls user's constructor, and passes it the username
     }
 
     Student(String username, String currentClass) {
         super(username);
+        //The "this" keyword refers to the member variable called currentClass inside of this class.
+        //Because I am using the same name for the variable I am passing into the constructor,
+        //I need to use the "this" keyword to differentiate between the two.
         this.currentClass = currentClass;
     }
 
-    //inherits the getUsername method
+    //inherits the getUsername method from User
 
     //add a method just to Student called getCurrentClass
     public String getCurrentClass() {
         return currentClass;
     }
-
 }
 
 /*****************************************************
